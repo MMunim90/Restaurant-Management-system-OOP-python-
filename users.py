e926044f0196c66c409e853cb93b10f2b6cdcd38
MMunim <mmunim9.9.01@gmail.com>
2026-04-30 02:23:09 +0600

    added abstruct base class User

---
+++ users.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+class User(ABC):
+    def __init__(self, name, phone, email, address):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+
