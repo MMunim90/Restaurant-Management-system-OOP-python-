b4012368dfe2245503da620fce608b3a29e6fbbd
MMunim <mmunim9.9.01@gmail.com>
2026-04-30 15:22:34 +0600

    build employee class

--- users.py
+++ users.py
@@ -7,3 +7,22 @@ class User(ABC):
         self.email = email
         self.address = address
 
+
+class Employee(User):
+    def __init__(self, name, phone, email, address,  age, designation, salary):
+        super().__init__(name, phone, email, address)
+        self.age = age
+        self.designation = designation
+        self.salary = salary
+
+
+# emp = Employee('Munim', 'munim@gmail.com', 123434, 'Dhaka', 23, 'chef', 12000)
+# print(emp.name)
+
+
+class Admin(User):
+    def __init__(self, name, phone, email, address):
+        super().__init__(name, phone, email, address)
+        self.employees = [] # Database
+
+    
